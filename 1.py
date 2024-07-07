5c011c7c43161b7dc1ac36be395889070964c2ba
Damir Mihajlović <74556243+damirko881@users.noreply.github.com>
2024-07-07 19:22:52 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,3 @@
-#Napisi program koji ispisuje cjelobrojne elemente koji se pojavljuju 3 ili vise puta unutar
-#liste. Generiraj listu od 100 elemenata u rasponu vrijednosti od 0 do 30. 
 import random
 from collections import Counter
 lista=[]
@@ -8,6 +6,9 @@ for x in range (0,100):
 
 elementi=Counter(lista)
 
-rezultat=[broj for broj, brojanje in elementi.items() if brojanje>=1]
+rezultat = []
+for broj, brojanje in elementi.items():
+    if brojanje >= 1:
+        rezultat.append(broj)
 
 print(rezultat)
